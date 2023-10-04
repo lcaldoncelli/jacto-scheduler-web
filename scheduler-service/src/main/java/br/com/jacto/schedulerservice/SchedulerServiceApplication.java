a81b0ccf112437fe96437e1ee15a10892da5079c
Lucas Caldoncelli Rodrigues <lcaldoncelli@gmail.com>
2023-10-04 17:08:04 -0300

    Startup Listener to add data do database based on a Configuration

--- scheduler-service/src/main/java/br/com/jacto/schedulerservice/SchedulerServiceApplication.java
+++ scheduler-service/src/main/java/br/com/jacto/schedulerservice/SchedulerServiceApplication.java
@@ -1,15 +1,29 @@
 package br.com.jacto.schedulerservice;
 
+import br.com.jacto.schedulerservice.configuration.SchedulerServiceConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Slf4j
 public class SchedulerServiceApplication {
-
+	@Autowired
+	SchedulerServiceConfiguration schedulerServiceConfiguration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulerServiceApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void onApplicationReady() {
+		if (schedulerServiceConfiguration.isDbInit()) {
+			log.info("Add default initial data");
+		} else {
+			log.info("Don't add default initial data");
+		}
+	}
 }
---
+++ scheduler-service/src/main/java/br/com/jacto/schedulerservice/configuration/SchedulerServiceConfiguration.java
@@ -0,0 +1,12 @@
+package br.com.jacto.schedulerservice.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class SchedulerServiceConfiguration {
+    @Value("${app.db.init:#{false}}")
+    private boolean dbInit;
+}
