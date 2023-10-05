2f0602d8a71979f92213607cb48865959dc59e1b
Lucas Caldoncelli Rodrigues <lcaldoncelli@gmail.com>
2023-10-05 15:55:35 -0300

    Gateway-Service initial commit

---
+++ gateway-service/src/main/java/br/com/jacto/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package br.com.jacto.gatewayservice;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+}
