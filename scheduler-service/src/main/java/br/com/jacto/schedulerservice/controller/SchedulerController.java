4b8ec24100b5151efa1b40d6a100030b7bfdb813
Lucas Caldoncelli Rodrigues <lcaldoncelli@gmail.com>
2023-10-05 16:15:34 -0300

    Port and route improvements

--- scheduler-service/src/main/java/br/com/jacto/schedulerservice/controller/SchedulerController.java
+++ scheduler-service/src/main/java/br/com/jacto/schedulerservice/controller/SchedulerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/scheduler")
+@RequestMapping("/scheduler")
 public class SchedulerController {
     @Autowired
     private SchedulerService schedulerService;
